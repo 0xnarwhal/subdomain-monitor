a6d0b90e2bd654e478162fb2ba8abaa6b365d58f
Amir Adrian <amiradrian@protonmail.com>
2023-09-17 22:24:31 +0800

    Completed MVP

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# subdomain-monitor
-Subdomain Monitoring Discord Bot
+# Subdomain Monitoring Bot
+
+## Table of Contents
+
+1. About the Project
+2. Getting Started
+3. Usage
+4. Future QOL Improvements
+5. License
+6. Contact
+
+## About the Project
+
+This is a simple bot that monitors the subdomains of a given domain and reports for any changes in the subdomains. It is built using the Python programming language and the Requests module. The bot is designed to be run on a server that you are hosting. I recommend using a VPS or dedicated server such as DigitalOcean (which is what I used).
+
+The bot will perform the following tasks every week upon running:
+
+1. Monitor the subdomains of a given domain.
+2. Report any changes in the subdomains Through a notification through the seleted Discord text channel.
+3. Log updated subdomains to a file.
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps. This assumes you have Python 3.8 or higher installed on your machine.
+
+### Installation
+
+- Clone the repository.
+- OPTIONAL: Create a virtual environment.
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+- Install the required modules.
+
+```bash
+pip install -r requirements.txt
+```
+
+- Create a file called `config.json` and add the following template and fill in the required information.
+
+```json
+{
+    "BOT_TOKEN": "<DISCORD_BOT_TOKEN>", 
+    "PREFIX": "!",
+    "DOMAIN_LIST": ["google.com","bing.com"],
+    "SAVE_LOCATION": "./results",
+    "API_KEY": "<WHOISXML_API_KEY>"
+}
+```
+
+## Usage
+
+- Run the bot.
+
+```bash
+python3 bot.py
+```
+
+- Run `!start` commmand in preffered Discord text channel to start monitoring.
+
+## Future QOL Improvements
+
+- Utilize embedded messages for better formatting instead of individual messages.
+- Add more functionality to the bot.
+- Add more error handling.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+Amir Adrian - amiradrian@protonmail.com
--- bot.py
+++ bot.py
@@ -4,61 +4,79 @@ import json
 import requests
 import os
 
+# Check if config.json exist. If not, will create a template config.json and exit.
+if os.path.isfile("./config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+else:
+    config_template =   {
+                            "BOT_TOKEN": "<DISCORD_BOT_TOKEN>", 
+                            "PREFIX": "!",
+                            "DOMAIN_LIST": ["google.com","bing.com"],
+                            "SAVE_LOCATION": "./results",
+                            "API_KEY": "<WHOISXML_API_KEY>"
+                        }
+    with open("./config.json", "w") as f:
+        json.dump(config_template, f, indent=4)
+    print("'config.json' file not found. Read the README.md file for instructions. 'config.json' file template has been created. Edit as you see fit. Exiting...")
+    exit()
 
-
-# Load settings.
-with open("./config.json") as f:
-    configData = json.load(f)
-
-# Create variables
-token = configData["BOT_TOKEN"]
+# Load config data from config.json file and assign them to variables.
+bot_token = configData["BOT_TOKEN"]
 prefix = configData["PREFIX"]
-domains = configData["DOMAIN_LIST"]
-api_token = configData["API_KEY"]
+domain_list = configData["DOMAIN_LIST"]
+api_key = configData["API_KEY"]
 save_location = configData["SAVE_LOCATION"]
-apireq = f"https://subdomains.whoisxmlapi.com/api/v1?apiKey={api_token}&domainName="
+req_url = f"https://subdomains.whoisxmlapi.com/api/v1?apiKey={api_key}&domainName="
 
 # If results save location doesn't exist, make one.
-if os.path.isdir('results'):
-    pass
+if os.path.isdir(save_location):
+    print(f"Results will be saved to {save_location}")
 else:
-    os.mkdir('results')
+    print(f"{save_location} doesn't exist. Creating one...")
+    os.mkdir(save_location)
+    print(f"Results will be saved to {save_location}")
 
 # Create bot instance
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
+# Logging message when bot is online.
 @bot.event
 async def on_ready():
     print("Bot is getting ready...")
-    await bot.change_presence(activity=discord.Game(name=f"with cum."))
-    print(f'{bot.user} is up!')
+    await bot.change_presence(activity=discord.Game(name=f"with cum.")) # Just for fun. Might want to remove this :)
+    print(f'{bot.user} is up and running!')
 
+# Commands to start and stop the scan task.
 @bot.command()
 async def start(ctx):
-    await ctx.send("Scan Started!")
     check_domains.start(ctx)
+    await ctx.send("Scan Started!")
 
 @bot.command()
 async def stop(ctx):
     check_domains.cancel()
     await ctx.send("Scan Stopped!")
 
-@tasks.loop(minutes=60)
+# Scan task that runs every 168 hours or 1 week.
+@tasks.loop(hours=168)
 async def check_domains(ctx):
-    await ctx.send("Scan Ongoing!")
+    await ctx.send("Scan is running...")
     
-    for domain in domains:
-        # Get new results
+    # Go through each domain in the list.
+    for domain in domain_list:
+        
+        # Fetch new results
         await ctx.send(f"{domain}: Fetching new results...")
-        new_results= requests.get(f"{apireq}{domain}").json()
+        new_results = requests.get(f"{req_url}{domain}").json()
         await ctx.send(f"{domain}: Received new results.")
 
-        # Check for new entries
-        if os.path.exists(f"{save_location}{domain}_old.json"):
+        # Check if old results exist. If not, print out the new results and save them to a file as old results.
+        # If old results exist, cross-reference them with new results and print out the new results that are not in the old results.
+        if os.path.exists(f"{save_location}/{domain}_old.json"):
             await ctx.send(f"{domain}: Cross-referencing with old results...")
             with open(f'{save_location}{domain}_old.json', 'r') as f:
                 old_results = json.load(f)
-        
             for entry in new_results["result"]["records"]:
                 subdomain = entry["domain"]
                 if not any(old_entry['domain'] == subdomain for old_entry in old_results["result"]["records"]):
@@ -70,11 +88,11 @@ async def check_domains(ctx):
                 await ctx.send(f"New entry: {subdomain}")
     
         # Save updated results
-        await ctx.send(f"Saving new results to {save_location}{domain}_old.json...")
-        with open(f'{save_location}{domain}_old.json', 'w') as f:
+        await ctx.send(f"Saving new results to {save_location}/{domain}_old.json...")
+        with open(f'{save_location}/{domain}_old.json', 'w') as f:
             json.dump(new_results, f, ensure_ascii=False, indent=4)
-        await ctx.send(f"Saved new results to {save_location}{domain}_old.json.")
+        await ctx.send(f"Saved new results to {save_location}/{domain}_old.json.")
     
     await ctx.send("Scan Complete!")
 
-bot.run(token)
+bot.run(bot_token)
