5ba1a0a5f285cbfec2ae0a807a1d55345583c5dc
Amir Adrian <amiradrian@protonmail.com>
2023-09-19 23:22:10 +0800

    Updated. Added QOL Updates. Logging Capabilities.

--- bot.py
+++ bot.py
@@ -1,98 +1,120 @@
-import discord
-from discord.ext import commands, tasks
-import json
-import requests
-import os
+import discord # Discord Library
+from discord.ext import commands, tasks # Discord Extensions
+import json # JSON Library. Used to read and write config.json file.
+import requests # Used to send HTTP requests to the API.
+import os # Used to verify files and directories.
+import datetime # Used for logging.
 
-# Check if config.json exist. If not, will create a template config.json and exit.
-if os.path.isfile("./config.json"):
-    with open("./config.json") as f:
-        configData = json.load(f)
-else:
-    config_template =   {
-                            "BOT_TOKEN": "<DISCORD_BOT_TOKEN>", 
-                            "PREFIX": "!",
-                            "DOMAIN_LIST": ["google.com","bing.com"],
-                            "SAVE_LOCATION": "./results",
-                            "API_KEY": "<WHOISXML_API_KEY>"
-                        }
-    with open("./config.json", "w") as f:
-        json.dump(config_template, f, indent=4)
-    print("'config.json' file not found. Read the README.md file for instructions. 'config.json' file template has been created. Edit as you see fit. Exiting...")
-    exit()
+# Check logs.txt existence
+if not os.path.isfile("./logs.txt"):
+    with open("./logs.txt", "w") as logs:
+        logs.write(f"[{datetime.datetime.now()}] Logs file not found. Creating one...\n")
+        logs.write(f"[{datetime.datetime.now()}] Logs file created. Bot starting...\n")
+else: # If logs.txt exist, do nothing.
+    with open("./logs.txt", "a") as logs:
+        logs.write(f"[{datetime.datetime.now()}] Logs file created. Bot starting...\n")
 
-# Load config data from config.json file and assign them to variables.
-bot_token = configData["BOT_TOKEN"]
-prefix = configData["PREFIX"]
-domain_list = configData["DOMAIN_LIST"]
-api_key = configData["API_KEY"]
-save_location = configData["SAVE_LOCATION"]
-req_url = f"https://subdomains.whoisxmlapi.com/api/v1?apiKey={api_key}&domainName="
+# Check config.json existence
+with open("./logs.txt", "a") as logs:
+    logs.write(f"[{datetime.datetime.now()}] Checking config.json...\n")
+    if os.path.isfile("./config.json"): # If config.json exist, load configuration.
+        logs.write(f"[{datetime.datetime.now()}] config.json found. Loading configuration...\n")
+        with open("./config.json") as config:
+            configData = json.load(config)
+            # Assign variables from config.json
+            bot_token = configData["BOT_TOKEN"]
+            prefix = configData["PREFIX"]
+            domain_list = configData["DOMAIN_LIST"]
+            api_key = configData["API_KEY"]
+            save_location = configData["SAVE_LOCATION"]
+            req_url = f"https://subdomains.whoisxmlapi.com/api/v1?apiKey={api_key}&domainName="
+        logs.write(f"[{datetime.datetime.now()}] Configuration loaded.\n")
+    else: # If config.json does not exist, create a template config.json and exit.
+        logs.write(f"[{datetime.datetime.datetime.now()}] config.json not found. Creating sample...\n")
+        config_template =   {
+                                "BOT_TOKEN": "<DISCORD_BOT_TOKEN>", 
+                                "PREFIX": "!",
+                                "DOMAIN_LIST": ["google.com","bing.com"],
+                                "SAVE_LOCATION": "./results",
+                                "API_KEY": "<WHOISXML_API_KEY>"
+                            }
+        with open("./config.json", "w") as config:
+            json.dump(config_template, config, indent=4)
+        logs.write(f"[{datetime.datetime.now()}] Sample config.json created. Exiting...\n")
+        exit()
 
 # If results save location doesn't exist, make one.
-if os.path.isdir(save_location):
-    print(f"Results will be saved to {save_location}")
-else:
-    print(f"{save_location} doesn't exist. Creating one...")
-    os.mkdir(save_location)
-    print(f"Results will be saved to {save_location}")
+with open("./logs.txt", "a") as logs:
+    logs.write(f"[{datetime.datetime.now()}] Checking save location...\n")
+    if os.path.isdir(save_location):
+        logs.write(f"[{datetime.datetime.now()}] Save location found. Continuing...\n")
+    else:
+        logs.write(f"[{datetime.datetime.now()}] Save location not found. Creating one...\n")
+        os.mkdir(save_location)
+        logs.write(f"[{datetime.datetime.now()}] Save location created.\n")
 
 # Create bot instance
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
-# Logging message when bot is online.
 @bot.event
 async def on_ready():
-    print("Bot is getting ready...")
-    await bot.change_presence(activity=discord.Game(name=f"with cum.")) # Just for fun. Might want to remove this :)
-    print(f'{bot.user} is up and running!')
+    with open("./logs.txt", "a") as logs:
+        logs.write(f"[{datetime.datetime.now()}] Bot starting...\n")
+        await bot.change_presence(activity=discord.Game(name=f"with cum.")) # Just for fun. Might want to remove this :)
+        logs.write(f"[{datetime.datetime.now()}] Bot has successfully started!\n")
 
 # Commands to start and stop the scan task.
 @bot.command()
 async def start(ctx):
-    check_domains.start(ctx)
-    await ctx.send("Scan Started!")
+    with open("./logs.txt", "a") as logs:
+        logs.write(f"[{datetime.datetime.now()}] {ctx.author} has started the scan task.\n")
+        check_domains.start(ctx)
+
 
 @bot.command()
 async def stop(ctx):
-    check_domains.cancel()
-    await ctx.send("Scan Stopped!")
+    with open("./logs.txt", "a") as f:
+        check_domains.cancel()
+        logs.write(f"[{datetime.datetime.now()}] {ctx.author} has stopped the scan task.\n")
 
 # Scan task that runs every 168 hours or 1 week.
 @tasks.loop(hours=168)
 async def check_domains(ctx):
-    await ctx.send("Scan is running...")
-    
-    # Go through each domain in the list.
-    for domain in domain_list:
+    with open("./logs.txt", "a") as logs:
+        logs.write(f"[{datetime.datetime.now()}] Scan task has started.\n")
         
-        # Fetch new results
-        await ctx.send(f"{domain}: Fetching new results...")
-        new_results = requests.get(f"{req_url}{domain}").json()
-        await ctx.send(f"{domain}: Received new results.")
+        # Go through each domain in the list.
+        for domain in domain_list:
+            # Fetch new results
+            logs.write(f"[{datetime.datetime.now()}] Fetching new results for {domain}...\n")
+            new_results = requests.get(f"{req_url}{domain}").json()
+            logs.write(f"[{datetime.datetime.now()}] Received new results for {domain}.\n")
 
-        # Check if old results exist. If not, print out the new results and save them to a file as old results.
-        # If old results exist, cross-reference them with new results and print out the new results that are not in the old results.
-        if os.path.exists(f"{save_location}/{domain}_old.json"):
-            await ctx.send(f"{domain}: Cross-referencing with old results...")
-            with open(f'{save_location}{domain}_old.json', 'r') as f:
-                old_results = json.load(f)
-            for entry in new_results["result"]["records"]:
-                subdomain = entry["domain"]
-                if not any(old_entry['domain'] == subdomain for old_entry in old_results["result"]["records"]):
-                    await ctx.send(f"New entry: {subdomain}")
-        else:
-            await ctx.send(f"{domain}: No old results found. Sending all new results.")
-            for entry in new_results["result"]["records"]:
-                subdomain = entry["domain"]
-                await ctx.send(f"New entry: {subdomain}")
-    
-        # Save updated results
-        await ctx.send(f"Saving new results to {save_location}/{domain}_old.json...")
-        with open(f'{save_location}/{domain}_old.json', 'w') as f:
-            json.dump(new_results, f, ensure_ascii=False, indent=4)
-        await ctx.send(f"Saved new results to {save_location}/{domain}_old.json.")
-    
-    await ctx.send("Scan Complete!")
+            # Check if old results exist. If not, print out the new results and save them to a file as old results.
+            # If old results exist, cross-reference them with new results and print out the new results that are not in the old results.
+            if os.path.exists(f"{save_location}/{domain}_old.json"):
+                logs.write(f"[{datetime.datetime.now()}] Old results found for {domain}. Cross-referencing with new results...\n")
+                with open(f'{save_location}{domain}_old.json', 'r') as oldData:
+                    old_results = json.load(oldData)
+                for entry in new_results["result"]["records"]:
+                    subdomain = entry["domain"]
+                    if not any(old_entry['domain'] == subdomain for old_entry in old_results["result"]["records"]):
+                        logs.write(f"[{datetime.datetime.now()}] New entry found: {subdomain}. Sending a notification to Discord...\n")
+                        embed=discord.Embed(title=f"http://{subdomain}", url=f"http://{subdomain}", description=f"A new subdomain has been found. Performing a small test should do it >:)", color=0xFF5733)
+                        await ctx.send(embed=embed)
+            else:
+                logs.write(f"[{datetime.datetime.now()}] No old results found for {domain}. Printing new results to Discord...\n")
+                for entry in new_results["result"]["records"]:
+                    subdomain = entry["domain"]
+                    embed=discord.Embed(title=f"http://{subdomain}", url=f"http://{subdomain}", description=f"A new subdomain has been found. Performing a small test should do it >:)", color=0xFF5733)
+                    await ctx.send(embed=embed)
+        
+            # Save updated results to a file.
+            logs.write(f"[{datetime.datetime.now()}] Saving new results to {save_location}/{domain}_old.json...\n")
+            with open(f'{save_location}/{domain}_old.json', 'w') as oldData:
+                json.dump(new_results, oldData, ensure_ascii=False, indent=4)
+            logs.write(f"[{datetime.datetime.now()}] Saved new results to {save_location}/{domain}_old.json.\n")
+        
+        logs.write(f"[{datetime.datetime.now()}] Scan task has ended.\n")
 
 bot.run(bot_token)
